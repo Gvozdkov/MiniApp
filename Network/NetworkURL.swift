2119d81472037c306407239f39de1ab3bd62b937
onePoint <aagvozdkov@yandex.ru>
2024-09-07 10:33:10 +0300

    Map
    - Настроен WKWebView для отображения веб-страницы с картами Яндекса
    - Добавлены методы для настройки контроллера и загрузки URL в WKWebView
    - Реализована функция загрузки карты через URLRequest

--- Network/NetworkURL.swift
+++ Network/NetworkURL.swift
@@ -10,16 +10,21 @@ import Foundation
 enum NetworkURL {
     case miniApps
     case weather(city: String)
+    case map
     
     var url: URL? {
         switch self {
         case .miniApps:
             return URL(string: "https://run.mocky.io/v3/5fe3b1f7-65a1-4c31-9715-aaa3feab0ebb")
+            
         case .weather(let city):
             let apiKey = "9e3c2f666b0e284e9d40e0331813110f"
             let baseUrl = "https://api.openweathermap.org/data/2.5/weather"
             let urlString = "\(baseUrl)?q=\(city)&appid=\(apiKey)&units=metric"
             return URL(string: urlString)
+            
+        case .map:
+            return URL(string: "https://yandex.ru/maps")
         }
     }
 }
